9caac1c8ce63a396a5e1a7ea98f2ccac80809cfe
FCPongs <metherelax@gmail.com>
2025-07-16 21:09:17 +0800

    feat: added dtb connection and api for pets

--- pet-monitoring-system/src/app/page.tsx
+++ pet-monitoring-system/src/app/page.tsx
@@ -1,5 +1,9 @@
 export default function Home() {
   return (
-    <div></div>
+    <>
+        <div className="w-full h-[100vh] text-2xl font-bold flex items-center bg-blue-100 justify-center">Pet Monitoring System</div>
+        <div></div>
+    </>
+
   );
 }
--- pet-monitoring-system/src/lib/connectMongodb.ts
+++ pet-monitoring-system/src/lib/connectMongodb.ts
@@ -1,15 +1,14 @@
 import mongoose from 'mongoose';
+const MONGO_URI = process.env.MONGO_URI;
 
-const MONGO_URI = process.env.MONGU_URI || "";
 if(!MONGO_URI){
-    throw new Error("Please define the MONGO_URI environment variable")
+    throw Error("Please add a URI in the env");
 }
 
-export const dbConnect = async (): Promise<void> => {
+export const connectDb = async ():Promise<void> => {
     try{
-        mongoose.connect(MONGO_URI);
-    }catch(error: unknown){
-        console.log(error)
+        await mongoose.connect(MONGO_URI);
+    } catch (error:unknown){
         throw error;
     }
 }
