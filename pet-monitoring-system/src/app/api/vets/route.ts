1285730847085f411faf3792ec23e662794bbef2
FCPongs <metherelax@gmail.com>
2025-07-18 23:08:26 +0800

    feat: added post api for vet

--- pet-monitoring-system/src/app/api/vets/route.ts
+++ pet-monitoring-system/src/app/api/vets/route.ts
@@ -2,12 +2,28 @@ import { connectDb } from "@/lib/connectMongodb";
 import { NextRequest, NextResponse } from "next/server";
 import { Vet } from "@/models/vets/vet";
 
-export const GET = async () =>{
+export const GET = async () => {
     await connectDb;
-    try{
+    try {
         const vets = await Vet.find();
         return NextResponse.json(vets);
-    }catch(error:unknown){
+    } catch (error: unknown) {
         throw error;
+    }
+}
+
+export const POST = async (req: NextRequest) => {
+    await connectDb();
+    try {
+        const vet = await req.json();
+        await Vet.create(vet);
+        return NextResponse.json(
+            { message: "Vet created", data: vet },
+            { status: 201 }
+        )
+    } catch (error: unknown) {
+        return NextResponse.json(
+            {message: "Failed to add vet", error: error}
+        )
     }
 }
