56ba1864b4fe888f8a80ce9d0fd1850835092713
FCPongs <metherelax@gmail.com>
2025-10-24 12:41:36 +0800

    refactor: modified schedule schema to cater custom dates

--- pet-monitoring-system/src/app/models/schedule.ts
+++ pet-monitoring-system/src/app/models/schedule.ts
@@ -1,32 +1,27 @@
 import mongoose from "mongoose";
 
 const { Schema, models } = mongoose;
-
-const TimeMedicationSchema = new Schema({
-    scheduleID: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'Schedule'
-    },
-    name: { type: String, required: [true, ""] },
-    time: { type: String, required: [true, "Time is required"] },
-    note: { type: String },
-    medication: {
-        medName: { type: String, required: [true, "Please input medication name"] },
-        dosage: { type: String },
-        drugType: { type: String }
-    }
-})
-
-
 const scheduleSchema = new Schema({
-    timeMedication: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: "TimeMedication",
-        required: true
-    },
+    name: { type: String, required: [true, ""] },
     startDate: { type: Date, required: true },
     endDate: { type: Date, required: true },
+    defaultMedication: [{
+        time: { type: String, required: true },
+        medication: {
+            medName: { type: String, required: true },
+            dosage: { type: String, required: true },
+            drugType: { type: String, required: true },
+        }
+    }],
+    customedMedication: [{
+        date: { type: [Date], required: true },
+        time: { type: String, required: true },
+        medication: {
+            medName: { type: String, required: true },
+            dosage: { type: String, required: true },
+            drugType: { type: String, required: true },
+        }
+    }]
 })
 
-export const Schedule = models.Schedule || mongoose.model("Schedule", scheduleSchema)
-export const TimeMedication = models.TimeMedication || mongoose.model("TimeMedication", TimeMedicationSchema);
+export const Schedule = models.Schedule || mongoose.model("Schedule", scheduleSchema);
