b0b4de0607c8db4dadd3ca3fea93e462cec010b4
FCPongs <metherelax@gmail.com>
2025-11-04 13:58:31 +0800

    schema: edited schedule schema, to make one time take multiple medicine, also changed date validation.

---
+++ pet-monitoring-system/src/app/validator/scheduleSchema.ts
@@ -0,0 +1,30 @@
+import * as z from "zod";
+
+const medicationSchema = z.object({
+    medName: z.string().min(1, "Please add a medication name"),
+    dosage: z.string().min(1, "Please add a dosage"),
+    drugType: z.string().min(1, "Please add a drug type")
+})
+const defaultMedicationSchema = z.object({
+    time: z.string().min(1, "Time is required"),
+    medication: z.array(medicationSchema).min(1, "Please add at least 1 medication")
+})
+const customMedicationSchema = z.object({
+  date: z.array(z.string()).min(1, "At least one date is required"),
+  time: z.string().min(1, "Time is required"),
+  medication: z.array(medicationSchema),
+});
+
+export const schedValidate = z.object(
+    {
+        _id: z.string().optional(),
+        name: z.string().min(1, "Please enter a name"),
+        startDate: z.date(),
+        endDate: z.date(),
+        defaultMedication: z.array(defaultMedicationSchema),
+        customMedication: z.array(customMedicationSchema).optional(),
+        timesPerDay: z.number().optional(),
+    }
+)
+
+export type Sched = z.infer<typeof schedValidate> ;
